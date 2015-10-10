78a0d91cc5b603d674c94cc2777efa9ecbf20e7b
Kendrick Parks <kendrick@homdna.com>
2015-10-09 20:37:35 -0700

    added script to upload files to raspberry pi

--- sftpTest.js
+++ sftpTest.js
@@ -5,16 +5,36 @@
 		Client = require('ssh2').Client,
 		conn = new Client();
 
+	// fs.readFile('../../roms/snes/DonkeyKongCountry(USA).sfc', function (err, data) {
+	// 	if (err) {
+	// 		throw err;
+	// 	}
+	// 	console.log(data);
+	// });
+
 	function connectSftp (err, sftp) {
 		if (err) {
 			throw err;
 		}
 
-		sftp.readdir('./RetroPie/roms', function (err, list) {
-			if (err) throw err;
-			console.dir(list);
-			conn.end();
-		});
+		// sftp.readdir('./RetroPie/roms', function (err, list) {
+		// 	if (err) throw err;
+		// 	console.dir(list);
+		// 	conn.end();
+		// });
+
+		var readStream = fs.createReadStream('../../roms/snes/Tiny\ Toon\ Adventures\ -\ Buster\ Busts\ Loose!\ (USA).sfc');
+        var writeStream = sftp.createWriteStream('./RetroPie/roms/snes/Tiny\ Toon\ Adventures\ -\ Buster\ Busts\ Loose!\ (USA).sfc');
+
+        // what to do when transfer finishes
+        writeStream.on('close', function () {
+            console.log( '- file transferred' );
+            sftp.end();
+            conn.end();
+        });
+
+        // initiate transfer of file
+        readStream.pipe( writeStream );
 	} 
 
 	function onReady () {
